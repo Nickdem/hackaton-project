3b44e83f503557bfea9d9a25da139b37de822342
Nickdem <45073853+Nickdem@users.noreply.github.com>
2020-04-12 02:02:54 +0300

    Новости

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -33,6 +33,53 @@ const Home = () => {
     ))
   }
 
+  const [newsCounter, setNewsCounter] = useState(3)
+
+  // const renderNews = (a, c) => {
+  //   const newsList = []
+
+  //   for(let i = 0; i < a.length &&  i < c; i++){
+  //     newsList.push(
+  //       <div key={i} className={classes.News}>
+  //         <img src={a[i].img} width='150' height='150' alt="Изображение новости"/>  
+  //           <div style={{'flex': '1', marginLeft: '1%'}}>
+  //           <small>{a[i].date}</small>
+  //           <h3>{a[i].name}</h3>
+  //           <p>{a[i].par}</p>
+  //         </div>
+  //       </div>
+  //     )
+  //   }
+
+  //   return (newsList)
+  // }
+
+
+  const renderNews = (a, c) => {
+	  const nameProject = ["Безопасные и качественные автомобильные дороги","Культура","Цифровая экономика"];
+	  const news = [];
+      const datanews = a[nameProject[count]];
+	  for( let i = 0; i <= datanews.length && i < c; i++ ){
+		if(datanews[i]){
+				news.push(
+					<a href={"http://"+datanews[i].url}  className={classes.NewsLink} key={i}>
+					  <div className={classes.News}>
+						<img src={datanews[i].img_url} width='150' height='150' alt="Изображение новости" />                 
+						<div style={{'flex': '1', marginLeft: '1%'}}>
+						  <small>{datanews[i].date}&nbsp;{datanews[i].time}</small>
+						  <h3>{datanews[i].news}</h3>
+						</div>
+					  </div>
+					</a>
+				);
+		}else{
+				break;
+		}
+	  }
+	  return news;
+  }
+
+
   return (
     <>
       <div className={classes.Head}>
@@ -54,18 +101,15 @@ const Home = () => {
         </div>
         <h2 style={{'padding': '1% 2%', backgroundColor: '#f5f5f5', boxShadow: '0 0 5px grey'}}>Последние новости:</h2>
         {data.loading === false
-        ? data.news.map((news, index) => {
-            return(
-              <div key={index} className={classes.News}>
-                <img src={news.img} width='150' height='150' alt="Изображение новости"/>                 
-                <div style={{'flex': '1', marginLeft: '1%'}}>
-                  <small>{news.date}</small>
-                  <h3>{news.name}</h3>
-                  <p>{news.par}</p>
-                </div>
-              </div>
-            )
-          })
+        ? <div>
+            {renderNews(data.news[0], newsCounter)}
+            <button 
+              onClick={()=> setNewsCounter(newsCounter + 3)}
+              className={newsCounter >= data.news[0].length || data.news[0].length === 0 ? classes.NewsButtonHidden : classes.NewsButton}              
+            >
+              Показать ещё!
+            </button>
+          </div>                
         : null
         }
         <Footer />
